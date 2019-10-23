89293d1158b7a4adc4edbf7597559121c5770b1e
BenitoB <benitobarreiro89@gmail.com>
2019-10-23 09:40:35 +0200

    Finished spinnerPersonas

--- ListaPersonas/app/src/main/java/com/bentie/listapersonas/MainActivity.java
+++ ListaPersonas/app/src/main/java/com/bentie/listapersonas/MainActivity.java
@@ -3,8 +3,11 @@ package com.bentie.listapersonas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -29,6 +32,14 @@ public class MainActivity extends AppCompatActivity {
 
         CustomPeopleAdapter adapter = new CustomPeopleAdapter(this, people);
         lv.setAdapter(adapter);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Persona p = people.get(position);
+                Toast.makeText(MainActivity.this, p.getName() + " tiene " + p.getAge() + " años.", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
